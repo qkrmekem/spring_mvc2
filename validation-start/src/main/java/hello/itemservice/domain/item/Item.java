3208614ea765819df7e2b3633efb7f140af0da50
qkrmekem <magicofclown@naver.com>
2023-05-14 23:59:16 +0900

    groups 적용

--- validation-start/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation-start/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,17 +12,19 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000이상이 되도록 입력해주세요")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
+    // HTTP요청은 악의적으로 변경해서 요청(post맨 이용 등)할 수 있으므로 서버에서도 검증해야함
     private Long id;
 
-    @NotBlank //(message = "공백X")
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}) //(message = "공백X")
     private String itemName;
 
-    @NotNull
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
     @Range(min = 1000, max = 1000000)
     private Integer price;
 
     @NotNull
-    @Max(9999)
+    @Max(value = 9999, groups = {SaveCheck.class}) // 수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
