5fe3983dc1417c2667507011c3f28783948b3eca
qkrmekem <magicofclown@naver.com>
2023-05-25 05:05:47 +0900

    HandlerExceptionResolver 적용

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -2,18 +2,23 @@ package hello.exception;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
+import hello.exception.resolver.UserHandlerExceptionResolver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // 인터셉터 추가
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LogInterceptor())
@@ -22,7 +27,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/css/**","*.ico","/error", "/error-page/**"); // 오류 페이지 경로
     }
 
-//    @Bean
+    // HandlerExceptionResolver 추가
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
+    }
+
+    // 필터 추가
+    //    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
