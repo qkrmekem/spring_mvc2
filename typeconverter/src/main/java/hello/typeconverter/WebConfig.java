4289e1257a7f4f97ca6834e19e477973c47a9f72
qkrmekem <magicofclown@naver.com>
2023-06-04 02:50:31 +0900

    포매터 완료

--- typeconverter/src/main/java/hello/typeconverter/WebConfig.java
+++ typeconverter/src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,10 +14,14 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
+        // 주석처리
+        // 컨버터가 포매터보다 우선순위가 높기 떄문에 주석처리
         registry.addConverter(new StringToIpPortConverter());
-        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new StringToIntegerConverter());
         registry.addConverter(new IpPortToStringConverter());
-        registry.addConverter(new IntegerToStringConverter());
+//        registry.addConverter(new IntegerToStringConverter());
 
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
