b08bf107e191fc0b31e89de5ec8faac08fdbb551
qkrmekem <magicofclown@naver.com>
2023-06-07 23:58:38 +0900

    스프링 파일 업로드

--- upload/src/main/java/hello/upload/controller/ServletUploadControllerV2.java
+++ upload/src/main/java/hello/upload/controller/ServletUploadControllerV2.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,6 +56,15 @@ public class ServletUploadControllerV2 {
             InputStream inputStream = part.getInputStream();
             String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
             log.info("body={}", body);
+
+            // 파일 저장하기
+            if (StringUtils.hasText(part.getSubmittedFileName())) {
+                String fullPath = fileDir + part.getSubmittedFileName();
+                log.info("파일 저장 fullPath={}", fullPath);
+                part.write(fullPath);
+            }
+
+            inputStream.close();
         }
 
         return "upload-form";
